e95e1cbfe41530ff3653d5605449145e78239af3
Osama Arshad <osamafreelancer22@gmail.com>
2024-10-06 13:45:22 +0500

    updated day 25

--- Day25/04_js_is_singleThreaded/app.js
+++ Day25/04_js_is_singleThreaded/app.js
@@ -1,5 +1,16 @@
+// Single threaded one line exec at a time
+// synchronous nature
+
+console.log("Hello World");
+console.log("Hello World 2");
+
+// dealing with single threaded nature
+// asynchronous nature
 setTimeout(function () {
-  console.log("apna college");
+  console.log("sapna college");
+}, 2000);
+setTimeout(function () {
+  console.log("hello world");
 }, 2000);
 
 console.log("hello ...");
---
+++ Day25/05_callback_hell/app.js
@@ -0,0 +1,10 @@
+let h1 = document.querySelector("h1");
+function changeColor(color, delay) {
+  setTimeout(() => {
+    h1.style.color = color;
+  }, delay);
+}
+
+changeColor("red", 1000);
+changeColor("green", 2000);
+changeColor("blue", 3000);
