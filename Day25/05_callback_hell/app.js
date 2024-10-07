ca3bb2b5e429bef40a4695c99597f0da0b69fabf
Osama Arshad <osamafreelancer22@gmail.com>
2024-10-07 17:53:13 +0500

    updated callback hell

--- Day25/05_callback_hell/app.js
+++ Day25/05_callback_hell/app.js
@@ -1,10 +1,15 @@
 let h1 = document.querySelector("h1");
-function changeColor(color, delay) {
+function changeColor(color, delay, nextColorChange) {
   setTimeout(() => {
     h1.style.color = color;
+    if (nextColorChange) nextColorChange();
   }, delay);
 }
 
-changeColor("red", 1000);
-changeColor("green", 2000);
-changeColor("blue", 3000);
+// pehla color red kro jb red ho jaye tu green kro and then blue ya possible ho ha nextColorChange
+// callback ke help sa. yaha callback ma nesting ho rahe ha. Easy to understand nai h callback nesting is lia is ko callback hell bolta hn.
+changeColor("red", 1000, () => {
+  changeColor("green", 1000, () => {
+    changeColor("blue", 1000);
+  });
+});
