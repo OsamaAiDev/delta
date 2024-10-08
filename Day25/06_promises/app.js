a523aec6c192ac2fc77fb3812b0d3a9745e1bb9c
Osama Arshad <osamafreelacer22@gmail.com>
2024-10-07 23:57:06 -0700

    callback hell with db example

---
+++ Day25/06_promises/app.js
@@ -0,0 +1,38 @@
+function savetoDb(data, success, failure) {
+  internetSpeed = Math.floor(Math.random() * 10) + 1;
+  if (internetSpeed > 4) {
+    // console.log("data saved to db", data);
+    success();
+  } else {
+    // console.log("Data not saved internet speed is not stable");
+    failure();
+  }
+}
+
+savetoDb(
+  "apna school",
+  () => {
+    console.log("data was saved to db");
+    savetoDb(
+      "smit",
+      () => {
+        console.log("success2: data2 saved");
+        savetoDb(
+          "apna college",
+          () => {
+            console.log("success3: data3 saved");
+          },
+          () => {
+            console.log("failure3: data3 not saved");
+          }
+        );
+      },
+      () => {
+        console.log("failure2: data2 not saved");
+      }
+    );
+  },
+  () => {
+    console.log("weak connection data not saved to db");
+  }
+);
