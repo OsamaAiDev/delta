e99d46dcfa00f5984e6c4ae95a028483ce1ca522
osama <osamafreelancer22@gmail.com>
2024-11-08 21:07:49 +0500

    Day 15 practice of NaN undefined Unary Operator

---
+++ 15_Day/app.js
@@ -0,0 +1,12 @@
+let name = null;
+
+console.log(typeof name); //object
+
+let age;
+console.log(typeof age); // undefined
+
+console.log(typeof NaN); // number
+
+// post increment first use than change
+console.log(age++); // NaN
+console.log(age); // NaN
